d75cb63e6e11dd34a6ae1b58aad864fc369c6780
DAW1-A <DAW1-A@PC17>
2024-03-01 17:35:50 +0100

    -Cambio del nombre del array de "arrayColores" a "colores". -Introducción de línea de saludo antes de la lista de colores. -Cambio del símbolo de los elementos de la lista de colores.

--- ImpresoraColores/src/App.java
+++ ImpresoraColores/src/App.java
@@ -1,13 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         //Array de colores
-        String[] arrayColores = {"Rojo", "Naranja", "Amarillo",
+        String[] colores = {"Rojo", "Naranja", "Amarillo",
         		"Verde", "Azul", "Violeta"};
 
         //Imprimimos los colores por pantalla
+        System.out.println("¡Hola!");
         System.out.println("Lista de colores:");
         for (int i = 0; i < arrayColores.length; i++) {
-            System.out.print("■ ");
+            System.out.print("» ");
             System.out.println(arrayColores[i]);
         }
     }
